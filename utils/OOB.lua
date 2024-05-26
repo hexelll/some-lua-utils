d5e612eb8363941bc01dd938e9d44a2fa36cd942
hexelll <139350367+hexelll@users.noreply.github.com>
2024-05-26 19:06:42 +0200

    Update OOB.lua
    added comments

--- utils/OOB.lua
+++ utils/OOB.lua
@@ -1,4 +1,4 @@
-function Class(args)
+function Class(args)--creates a class from a table
     local self = args
     return function(oargs)
         for key,value in pairs(oargs) do
@@ -8,7 +8,7 @@ function Class(args)
     end
 end
 
-function Object(class,args)
+function Object(class,args)--instantiates an class with some arguments
     local self = {}
     for key,value in pairs(class(args)) do
         self[key] = value
@@ -25,6 +25,15 @@ function Object(class,args)
     end
     return self
 end
+--[[EXEMPLE USE:
+    CAR = Class({
+        drive = function(),
+        fuel = 100,
+        speed = 5
+    })
+    sportsCar = Object(CAR,{speed = 20,fuel = 50})
+    badSportsCar = sportsCar.copy({speed = 15,fuel = 25})
+--]]
 return {
     Class = Class,
     Object = Object
