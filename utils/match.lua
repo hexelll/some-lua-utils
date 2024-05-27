60189d1b0adcd9be7903601477ffd7dc65ebcd3d
hexelll <139350367+hexelll@users.noreply.github.com>
2024-05-27 23:40:25 +0200

    Update match.lua
    fixed match

--- utils/match.lua
+++ utils/match.lua
@@ -1,18 +1,19 @@
 function match(...)--first argument is the case to deal with other arguments are a table with a case and an associated function
     local done = false
+    local case = arg[1]
     local defFn = function()end
     for k,v in pairs(arg) do
         if type(v) == "table" then
-            if not(k==1) and v[1]==arg[1] then
-                v[2]()
+            if not(k==1) and v[1]==case and not(v[1]=="default") then
+                v[2](case)
                 done = true
             end
             if v[1] == "default" then
                 defFn = v[2]
             end
         end
-        if not done then defFn() end
     end
+    if not done then defFn(case) end
 end
 --[[ EXEMPLE USE:
     match(condition,
@@ -22,3 +23,4 @@ end
     )
 --]]
 return match
+
